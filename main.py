5b7efa5d302eee529440b40aef228478c7e1cac9
JeremyFamengo <jeremydj3@gmail.com>
2023-11-14 12:52:18 +0100

    removed case sesitive of extension for Windows

--- main.py
+++ main.py
@@ -9,21 +9,24 @@ from PIL import Image
 def compress_images(folder_path):
     for root, dirs, files in os.walk(folder_path):
         for file in files:
-            if file.endswith(".jpg") or file.endswith('.jpeg') or file.endswith(".png") or file.endswith(".JPG") or file.endswith('.JPEG') or file.endswith(".PNG"):
+            # Per Linux e Mac aggiungere (or file.endswith(".JPG") or file.endswith('.JPEG') or file.endswith(".PNG")) nella seguente condizione if
+            if file.endswith(".jpg") or file.endswith('.jpeg') or file.endswith(".png") :
                 print(os.listdir(root).index(file), " di ", os.listdir(root).__len__(), " -> ", file)
                 image_path = os.path.join(root, file)
                 img = Image.open(image_path)
                 if img.width > 1920 or img.height > 1080:
                     img.thumbnail((1920, 1080))
                     #img.save(image_path, quality=70)
-                    if file.endswith('.jpg') or file.endswith('.jpeg') or file.endswith('.JPG') or file.endswith('.JPEG'):
+                    # Per Linux e Mac aggiungere (or file.endswith('.JPG') or file.endswith('.JPEG')) nella seguente condizione if
+                    if file.endswith('.jpg') or file.endswith('.jpeg'):
                         img.save(os.path.join(root, 'compressed_' + file), 'JPEG', quality=70)
                     else:
                         img.save(os.path.join(root, 'compressed_' + file), 'PNG', quality=70)
 
 # Funzione che conta il numero di immagini presenti nella cartella da analizzare
 def count_image_files(directory):
-    image_extensions = ['*.jpg', '*.jpeg', '*.png', '*.JPG', '*.JPEG', '*.PNG'] #'*.gif', '*.bmp', '.heic' # Add more extensions if needed
+    # Per Linux e Mac aggiungere (, '*.JPG', '*.JPEG', '*.PNG') nell'array seguente
+    image_extensions = ['*.jpg', '*.jpeg', '*.png'] #'*.gif', '*.bmp', '.heic' # Add more extensions if needed
     count = 0
     for extension in image_extensions:
         count += len(glob.glob(os.path.join(directory, '**', extension), recursive=True))
@@ -32,6 +35,7 @@ def count_image_files(directory):
 
 if __name__ == '__main__':
     # info for the final user
+    print("*Warning: for Linux and Mac enable case sensitive by uncommenting relative comment lines in the code.*")
     print("This program reduce all photos with image resolution bigger than 1920x1080 (Full HD) to Full HD.")
     #directory_path = '/home/jeremy/Immagini/test_convert' # cartella da analizzare
     directory_path=input("Insert the folder where is contained the images (without the character \' or \" at the beginning or the end of the path): ")
